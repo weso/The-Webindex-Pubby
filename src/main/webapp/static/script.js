783609d0163a4e0617c01f6d8da30929a2583374
Alejandro <alejandro.montes.g@gmail.com>
2013-03-07 18:51:48 +0100

    Eliminated some unused files
    Eliminated some unised files since the SPARQL queries were moved to Java
    and the update of Foundation

--- src/main/webapp/static/script.js
+++ src/main/webapp/static/script.js
@@ -2,39 +2,6 @@ function init() {
     init_long_literals();
 }
 
-function setRank(fileName, rankClassName, scoreClassName) {
-    $.getJSON(
-     fileName,
-     { key: 'countryName', otherkey: true },
-     function (data) {
-         for (var i = 0; i < data.results.bindings.length; i++) {
-             if (document.getElementById("title").textContent === data.results.bindings[i].countryName.value) {
-                 Array.prototype.slice.call(document.getElementsByClassName(rankClassName), 0).forEach(function (n) {
-                     n.textContent = textContent = "" + (i + 1);
-                 });
-                 Array.prototype.slice.call(document.getElementsByClassName(scoreClassName), 0).forEach(function (n) {
-                     n.textContent = textContent = "" + Math.round(data.results.bindings[i].value.value * 100) / 100;
-                 });
-                 }
-             }
-         }
-);
-}
-
-function setFlag() {
-    var country_name = document.getElementById("title").textContent;
-    var query = 'select ?flag where {' +
-        '?c rdfs:label "' + country_name + '"@en .' +
-        '?c dbpedia-owl:thumbnail ?flag .' +
-        '} LIMIT 1';
-    var endpoint = "http://dbpedia.org/sparql";
-    function c(str) {
-        var jsonObj = eval('(' + str + ')');
-        document.getElementById("flag").setAttribute("src", jsonObj.results.bindings[0].flag.value);
-    }
-    sparqlQueryJson(query, endpoint, c);
-}
-
 var long_literal_counter = 0;
 var long_literal_spans = {};
 var long_literal_texts = {};
@@ -60,33 +27,6 @@ function init_long_literals() {
         long_literal_counter = long_literal_counter + 1;
     }
 }
-
-function sparqlQueryJson(queryStr, endpoint, callback) {
-    var querypart = "query=" + escape(queryStr);
-    var xmlhttp = null;
-    if (window.XMLHttpRequest) {
-        xmlhttp = new XMLHttpRequest();
-    } else if (window.ActiveXObject) {
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    } else {
-        alert('Perhaps your browser does not support XMLHttpRequests?');
-    }
-    xmlhttp.open('POST', endpoint, true);
-    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xmlhttp.setRequestHeader("Accept", "application/sparql-results+json");
-
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4) {
-            if (xmlhttp.status == 200) {
-                callback(xmlhttp.responseText);
-            } else {
-                console.log("Sparql query error: " + xmlhttp.status + " - "
-                    + xmlhttp.responseText);
-            }
-        }
-    };
-    xmlhttp.send(querypart);
-};
 
 function expand(i) {
     var span = long_literal_spans[i];
